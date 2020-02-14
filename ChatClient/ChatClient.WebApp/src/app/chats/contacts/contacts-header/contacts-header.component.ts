08a9511f95da525d1444662714bc75116bb78bdf
Andri Wandres <andrilucawandres@gmail.com>
2020-02-14 18:01:18 +0100

    implement contacts header

--- ChatClient/ChatClient.WebApp/src/app/chats/contacts/contacts-header/contacts-header.component.ts
+++ ChatClient/ChatClient.WebApp/src/app/chats/contacts/contacts-header/contacts-header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts-header',
   templateUrl: './contacts-header.component.html',
   styleUrls: ['./contacts-header.component.scss']
 })
-export class ContactsHeaderComponent implements OnInit {
+export class ContactsHeaderComponent {
+  @Output() readonly addClick = new EventEmitter<void>(true);
+  @Output() readonly menuClick = new EventEmitter<void>(true);
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  readonly searchControl = new FormControl('');
 }
--- ChatClient/ChatClient.WebApp/src/app/chats/contacts/contacts-header/contacts-header.module.ts
+++ ChatClient/ChatClient.WebApp/src/app/chats/contacts/contacts-header/contacts-header.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ContactsHeaderComponent } from './contacts-header.component';
-
-
 
 @NgModule({
   declarations: [ContactsHeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
   ],
   exports: [ContactsHeaderComponent]
 })
