d8ac5bf1eae9481f4df3b455061ae57dbea9e2b0
Andri Wandres <andrilucawandres@gmail.com>
2020-02-22 13:39:28 +0100

    load messages from store / disable compactFolders in IDE

--- ChatClient/ChatClient.WebApp/src/app/chats/chat/chat-messages/chat-messages.component.ts
+++ ChatClient/ChatClient.WebApp/src/app/chats/chat/chat-messages/chat-messages.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { AppStoreState } from 'src/app/app-store';
+import { ChatMessagesStoreSelectors, ChatMessagesStoreActions } from 'src/app/app-store/chat-messages-store';
 
 @Component({
   selector: 'app-chat-messages',
@@ -8,14 +11,28 @@ import { AppStoreState } from 'src/app/app-store';
   styleUrls: ['./chat-messages.component.scss']
 })
 export class ChatMessagesComponent implements OnInit, OnDestroy {
+  @Input() targetId: number;
+  @Input() isGroupChat: boolean;
+
+  readonly destroy$ = new Subject<void>();
+
+  readonly messages$ = this.store$.pipe(
+    select(ChatMessagesStoreSelectors.selectAll),
+    takeUntil(this.destroy$)
+  );
 
   constructor(private readonly store$: Store<AppStoreState.State>) { }
 
   ngOnInit(): void {
+    const action = this.isGroupChat
+      ? ChatMessagesStoreActions.loadGroupMessages({ groupId: this.targetId })
+      : ChatMessagesStoreActions.loadPrivateMessages({ recipientId: this.targetId });
 
+    this.store$.dispatch(action);
   }
 
   ngOnDestroy(): void {
-
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
