601f1eb890192dd8c1a32dd1d78232bb95df156f
Andri Wandres <andrilucawandres@gmail.com>
2020-10-13 19:44:35 +0200

    add utilities for repository-testing

---
+++ ChatClient/ChatClient.Tests/TestUtilities/DataContainer.cs
@@ -0,0 +1,26 @@
+﻿using ChatClient.Core.Models.Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatClient.Tests.TestUtilities
+{
+    public class DataContainer
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Message> Messages { get; set; }
+        public IEnumerable<MessageRecipient> MessageRecipients { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+        public IEnumerable<GroupMembership> GroupMemberships { get; set; }
+        public IEnumerable<UserRelationship> UserRelationships { get; set; }
+
+        public DataContainer()
+        {
+            Users = Enumerable.Empty<User>();
+            Groups = Enumerable.Empty<Group>();
+            Messages = Enumerable.Empty<Message>();
+            UserRelationships = Enumerable.Empty<UserRelationship>();
+            MessageRecipients = Enumerable.Empty<MessageRecipient>();
+            GroupMemberships = Enumerable.Empty<GroupMembership>();
+        }
+    }
+}
---
+++ ChatClient/ChatClient.Tests/TestUtilities/RepositoryTestsBase.cs
@@ -0,0 +1,43 @@
+﻿using ChatClient.Data.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient.Tests.TestUtilities
+{
+    public abstract class RepositoryTestsBase : IDisposable
+    {
+        protected ChatContext Context { get; private set; }
+
+        protected void SeedTestDatabase(DataContainer dataContainer)
+        {
+            DbContextOptions<ChatContext> options = new DbContextOptionsBuilder<ChatContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            Context = new ChatContext(options);
+
+            // Clean and re-create database
+            Context.Database.EnsureDeleted();
+            Context.Database.EnsureCreated();
+
+            // Add entities from mapping container
+            Context.Users.AddRange(dataContainer.Users);
+            Context.Groups.AddRange(dataContainer.Groups);
+            Context.Messages.AddRange(dataContainer.Messages);
+            Context.UserRelationships.AddRange(dataContainer.UserRelationships);
+            Context.MessageRecipients.AddRange(dataContainer.MessageRecipients);
+            Context.GroupMemberships.AddRange(dataContainer.GroupMemberships);
+
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            // Delete the test database
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+}
