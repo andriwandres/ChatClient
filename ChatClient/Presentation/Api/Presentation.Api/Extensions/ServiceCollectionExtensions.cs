f3252b025d8d51ba3eff83f98919dbbfab9d8ca2
Andri Wandres <andrilucawandres@gmail.com>
2020-11-02 20:30:07 +0100

    add swagger

---
+++ ChatClient/Presentation/Api/Presentation.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Presentation.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPresentationServices(this IServiceCollection services)
+        {
+            // Add swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("master", new OpenApiInfo
+                {
+                    Title = "Chat Client API",
+                    Description = "REST API for instant-messenger web client",
+                });
+
+                // Define jwt bearer authentication for securing the api in swagger
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+                {
+                    Description = "JWT authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                // Define jwt bearer as the security requirement
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = JwtBearerDefaults.AuthenticationScheme }
+                        },
+                        new string[] {}
+                    }
+                });
+
+                // Add xml documentation across all assemblies
+                Assembly currentAssembly = Assembly.GetExecutingAssembly();
+
+                Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+                string[] xmlPaths = assemblies
+                    .Select(assembly => Path.Combine(Path.GetDirectoryName(currentAssembly.Location) ?? string.Empty,
+                        $"{assembly.GetName().Name}.xml"))
+                    .Where(File.Exists)
+                    .ToArray();
+
+                foreach (string path in xmlPaths)
+                {
+                    options.IncludeXmlComments(path);
+                }
+
+                // Include validation rules from FluentValidation
+                options.AddFluentValidationRules();
+
+                // Add support for swagger examples
+                options.ExampleFilters();
+            });
+
+            // Add swagger example providers from assemblies
+            services.AddSwaggerExamplesFromAssemblyOf<Startup>();
+
+            // Add support for controllers
+            services.AddControllers();
+        }
+    }
+}
--- ChatClient/Presentation/Api/Presentation.Api/Startup.cs
+++ ChatClient/Presentation/Api/Presentation.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Presentation.Api.Extensions;
 
 namespace Presentation.Api
 {
@@ -25,7 +26,7 @@ namespace Presentation.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddPresentationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
