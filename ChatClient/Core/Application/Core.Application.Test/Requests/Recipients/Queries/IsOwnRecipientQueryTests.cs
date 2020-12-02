6e9cece137bba28d1363977c373469fe7ba6e0f8
Andri Wandres <andrilucawandres@gmail.com>
2020-12-02 23:35:52 +0100

    add tests for validation business logic

---
+++ ChatClient/Core/Application/Core.Application.Test/Requests/Recipients/Queries/IsOwnRecipientQueryTests.cs
@@ -0,0 +1,112 @@
+﻿using Core.Application.Database;
+using Core.Application.Requests.Recipients.Queries;
+using Core.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using MockQueryable.Moq;
+using Moq;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Core.Application.Test.Requests.Recipients.Queries
+{
+    public class IsOwnRecipientQueryTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+
+        public IsOwnRecipientQueryTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+            Claim nameIdentifierClaim = new Claim(ClaimTypes.NameIdentifier, 1.ToString());
+
+            _httpContextAccessorMock
+                .Setup(m => m.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier))
+                .Returns(nameIdentifierClaim);
+        }
+
+        [Fact]
+        public async Task IsOwnRecipientQueryHandler_ShouldReturnTrue_WhenRecipientIsOwnUser()
+        {
+            // Arrange
+            IsOwnRecipientQuery request = new IsOwnRecipientQuery { RecipientId = 1 };
+
+            IQueryable<Recipient> databaseRecipient = new[]
+            {
+                new Recipient { RecipientId = 1, UserId = 1 }
+            }
+            .AsQueryable()
+            .BuildMock()
+            .Object;
+
+            _unitOfWorkMock
+                .Setup(m => m.Recipients.GetById(request.RecipientId))
+                .Returns(databaseRecipient);
+
+            IsOwnRecipientQuery.Handler handler = new IsOwnRecipientQuery.Handler(_unitOfWorkMock.Object, _httpContextAccessorMock.Object);
+
+            // Act
+            bool isOwnRecipient = await handler.Handle(request);
+
+            // Assert
+            Assert.True(isOwnRecipient);
+        }
+
+        [Fact]
+        public async Task IsOwnRecipientQueryHandler_ShouldReturnFalse_WhenRecipientIsForeignUser()
+        {
+            // Arrange
+            IsOwnRecipientQuery request = new IsOwnRecipientQuery { RecipientId = 1 };
+
+            IQueryable<Recipient> databaseRecipient = new[]
+            {
+                new Recipient { RecipientId = 1, UserId = 2 }
+            }
+            .AsQueryable()
+            .BuildMock()
+            .Object;
+
+            _unitOfWorkMock
+                .Setup(m => m.Recipients.GetById(request.RecipientId))
+                .Returns(databaseRecipient);
+
+            IsOwnRecipientQuery.Handler handler = new IsOwnRecipientQuery.Handler(_unitOfWorkMock.Object, _httpContextAccessorMock.Object);
+
+            // Act
+            bool isOwnRecipient = await handler.Handle(request);
+
+            // Assert
+            Assert.False(isOwnRecipient);
+        }
+
+        [Fact]
+        public async Task IsOwnRecipientQueryHandler_ShouldReturnFalse_WhenRecipientIsForeignGroup()
+        {
+            // Arrange
+            IsOwnRecipientQuery request = new IsOwnRecipientQuery { RecipientId = 1 };
+
+            IQueryable<Recipient> databaseRecipient = new[]
+            {
+                new Recipient { RecipientId = 1, GroupMembershipId = 1 }
+            }
+            .AsQueryable()
+            .BuildMock()
+            .Object;
+
+            _unitOfWorkMock
+                .Setup(m => m.Recipients.GetById(request.RecipientId))
+                .Returns(databaseRecipient);
+
+            IsOwnRecipientQuery.Handler handler = new IsOwnRecipientQuery.Handler(_unitOfWorkMock.Object, _httpContextAccessorMock.Object);
+
+            // Act
+            bool isOwnRecipient = await handler.Handle(request);
+
+            // Assert
+            Assert.False(isOwnRecipient);
+        }
+    }
+}
---
+++ ChatClient/Core/Application/Core.Application.Test/Requests/Recipients/Queries/RecipientExistsQueryTests.cs
@@ -0,0 +1,57 @@
+﻿using Core.Application.Database;
+using Core.Application.Requests.Recipients.Queries;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Core.Application.Test.Requests.Recipients.Queries
+{
+    public class RecipientExistsQueryTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+
+        public RecipientExistsQueryTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public async Task RecipientExistsQueryHandler_ShouldReturnTrue_WhenRecipientExists()
+        {
+            // Arrange
+            RecipientExistsQuery request = new RecipientExistsQuery {RecipientId = 1};
+
+            _unitOfWorkMock
+                .Setup(m => m.Recipients.Exists(request.RecipientId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(true);
+
+            RecipientExistsQuery.Handler handler = new RecipientExistsQuery.Handler(_unitOfWorkMock.Object);
+
+            // Act
+            bool exists = await handler.Handle(request);
+
+            // Assert
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task RecipientExistsQueryHandler_ShouldReturnFalse_WhenRecipientDoesNotExist()
+        {
+            // Arrange
+            RecipientExistsQuery request = new RecipientExistsQuery { RecipientId = 1 };
+
+            _unitOfWorkMock
+                .Setup(m => m.Recipients.Exists(request.RecipientId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            RecipientExistsQuery.Handler handler = new RecipientExistsQuery.Handler(_unitOfWorkMock.Object);
+
+            // Act
+            bool exists = await handler.Handle(request);
+
+            // Assert
+            Assert.False(exists);
+        }
+    }
+}
