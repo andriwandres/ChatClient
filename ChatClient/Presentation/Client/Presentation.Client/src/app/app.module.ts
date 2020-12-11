cc244562cc640d30adf291e5681ddc3a7af15b18
Andri Wandres <andrilucawandres@gmail.com>
2020-12-11 11:52:30 +0100

    fix import error

--- ChatClient/Presentation/Client/Presentation.Client/src/app/app.module.ts
+++ ChatClient/Presentation/Client/Presentation.Client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth';
+import { AuthModule } from '@chat-client/auth';
 import { RootStoreModule } from './store/root-store.module';
 
 @NgModule({
