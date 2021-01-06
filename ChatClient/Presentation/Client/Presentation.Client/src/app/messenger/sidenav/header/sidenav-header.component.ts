205cbc314d89a5cb3041a65eb638342a97473ef6
Andri Wandres <andrilucawandres@gmail.com>
2021-01-06 22:44:19 +0100

    make sidenav closeable

--- ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/header/sidenav-header.component.ts
+++ ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/header/sidenav-header.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-header',
   templateUrl: './sidenav-header.component.html',
   styleUrls: ['./sidenav-header.component.scss']
 })
-export class SidenavHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SidenavHeaderComponent {
+  @Output() hamburgerMenuClick = new EventEmitter();
 }
--- ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/header/sidenav-header.module.ts
+++ ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/header/sidenav-header.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavHeaderComponent } from './sidenav-header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [SidenavHeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [SidenavHeaderComponent]
 })
