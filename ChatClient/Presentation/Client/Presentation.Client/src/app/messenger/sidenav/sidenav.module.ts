d0418a71c45606099c20f68a54fd4ab32cfd2f47
Andri Wandres <andrilucawandres@gmail.com>
2021-01-06 22:17:09 +0100

    create building blocks of messenger view

---
+++ ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/sidenav.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav.component';
+
+@NgModule({
+  declarations: [SidenavComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [SidenavModule]
+})
+export class SidenavModule { }
