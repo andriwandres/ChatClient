59891050c87343a74ab2b701fa398c0d2e5991b7
Andri Wandres <andrilucawandres@gmail.com>
2021-01-14 16:15:28 +0100

    add truncation of long messages inside recipient row

--- ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/recipients/recipient/recipient.component.ts
+++ ChatClient/Presentation/Client/Presentation.Client/src/app/messenger/sidenav/recipients/recipient/recipient.component.ts
@@ -4,8 +4,14 @@ import { Recipient } from '@chat-client/core/models';
 @Component({
   selector: 'app-recipient',
   templateUrl: './recipient.component.html',
-  styleUrls: ['./recipient.component.scss']
+  styleUrls: ['./recipient.component.scss'],
 })
 export class RecipientComponent {
   @Input() recipient!: Recipient;
+
+  // truncate(text: string, characters: number): string {
+  //   return text.length > characters
+  //     ? text.substring(0, characters - 1) + '...'
+  //     : text;
+  // }
 }
